e813d73b96e01d84ba7ef912566872b379e411a7
NikolajRFA <34486013+NikolajRFA@users.noreply.github.com>
2022-11-21 16:55:39 +0100

    Deleted comment.

--- src/Main.java
+++ src/Main.java
@@ -24,35 +24,5 @@ public class Main {
                 System.out.println("Loading " + tempCargo.getClass().getName() + " on to " + tempVessel.getClass().getName() + " vessel went bad.");
             }
         }
-
-        /*
-        Container container = new Container(20, 10, 2.5f, "DK", 60);
-        if (container.loadingCargo(new ContainerCargo(20))) {
-            System.out.println("Container loading went well");
-        } else {
-            System.out.println("Not enough space for that many containers!");
-        }
-        System.out.println("Container ship is: " + container.utilityLevelOfCapacity() + " full.");
-
-        System.out.println();
-
-        Tanker tanker = new Tanker(20, 10, 2.5f, "DK", 2);
-        if (tanker.loadingCargo(new TankerCargo(0, 10))) {
-            System.out.println("Tanker loading went well.");
-        } else {
-            System.out.println("Tanker loading went wrong!");
-        }
-        System.out.println("Tanker is: " + tanker.utilityLevelOfCapacity() + " full.");
-
-        System.out.println();
-
-        RoRo roRo = new RoRo(30, 10, 2.5f, "DK", 200);
-        if (roRo.loadingCargo(new RoRoCargo(4, 3))) {
-            System.out.println("RoRo loading went well.");
-        } else {
-            System.out.println("RoRo loading went wrong!");
-        }
-        System.out.println("RoRo has " + roRo.utilityLevelOfCapacity() + " meters filled.");
-        */
     }
 }
