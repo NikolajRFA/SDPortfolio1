f9cd119f3c2157d3b041e9b7ba5a2ea2483b7985
NikolajRFA <34486013+NikolajRFA@users.noreply.github.com>
2022-11-22 13:56:50 +0100

    Added comments.

--- src/Vessel.java
+++ src/Vessel.java
@@ -10,8 +10,11 @@ public abstract class Vessel {
         this.FLAG_NATION = flagNation;
     }
 
+    // Abstract classes to be implemented in the subclasses.
     public abstract double utilityLevelOfCapacity();
     public abstract boolean loadingCargo(Cargo cargo);
+
+    // Extension of system.
     public void sailTo(String coords) {}
     public String getCoordinates(){return null;}
     public static Vessel findSuitableVessel(Vessel[] vessels, String vesselType, int spaceNeeded) {
